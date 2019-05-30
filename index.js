ebdf1effb872681310135c02f5dcaeaa5302993d
gunnerdeath <jingkcheng@hotmail.com>
2019-05-30 10:20:04 -0400

    Files added

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+}); 
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+    socket.on('user login', function(user){
+        console.log ('User: ' + user + ' has joined the chat.');
+        io.emit('user login', user);
+    });
+    socket.on('chat message', function(msg){
+        console.log(msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('user logout', function(msg){
+        console.log(msg);
+        io.emit('user logout', msg);
+        
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
